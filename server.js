fd16bb5db5bf3e2b869dac9f89601b926f49e08a
benedikturcan <benedikt_urcan@hotmail.de>
2023-04-01 11:33:59 +0200

    build: adding mongo db and connected to db

--- server.js
+++ server.js
@@ -1,6 +1,52 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A service must have a name'],
+    unique: [true, 'A name with this service already exists'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park hiker',
+  rating: 5,
+  price: 1400,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error!', err);
+  });
 
 const app = require('./app');
 
